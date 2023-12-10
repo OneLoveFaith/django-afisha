9fc2c0a23a9e21bda5bf2cf608e80cd7d6b6f815
OneLoveFaith <oneloveshinobu@gmail.com>
2023-12-10 18:18:43 +0600

    Review, Rating, movie_count and ViewSet update

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
-from .views import DirectorList, DirectorDetail, MovieList, MovieDetail, ReviewList, ReviewDetail
+from rest_framework.routers import DefaultRouter
+from .views import DirectorsViewSet, MoviesViewSet, ReviewsViewSet
+
+default_router = DefaultRouter()
+default_router.register('directors', DirectorsViewSet, basename='director')
+default_router.register('reviews', ReviewsViewSet, basename='review')
+default_router.register('movies', MoviesViewSet, basename='movie')
+
 
 urlpatterns = [
-    path('directors/', DirectorList.as_view(), name='director-list'),
-    path('directors/<int:pk>/', DirectorDetail.as_view(), name='director-detail'),
-    path('movies/', MovieList.as_view(), name='movie-list'),
-    path('movies/<int:pk>/', MovieDetail.as_view(), name='movie-detail'),
-    path('reviews/', ReviewList.as_view(), name='review-list'),
-    path('reviews/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
+    # path('movies/', MovieList.as_view(), name='movie-list'),
+    # path('movies/<int:pk>/', MovieDetail.as_view(), name='movie-detail'),
+    # path('movies/reviews/', MovieReviewsList.as_view(), name='movie-reviews-list'),
+    # path('reviews/', ReviewList.as_view(), name='review-list'),
+    # path('reviews/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
+
 ]
+
+urlpatterns += default_router.urls
